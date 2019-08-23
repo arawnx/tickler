79492b2a1ab740a4bbd078bee7ca0cce39b345a0
xanderxylona <sashasylona@gmail.com>
2019-08-23 15:46:39 -0400

    Initial commit

---
+++ tickler.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+DAY=$(date +%d)
+HOME='/Users/xander'
+TICKLERDIR="$HOME/Documents/gtd/tickler"
+FILE="$TICKLERDIR/$YEAR/$MONTH/$DAY.txt"
+if [ -f $FILE ];
+then
+	cat $FILE
+else
+	echo "Nothing in the tickler to-day!"
+fi
